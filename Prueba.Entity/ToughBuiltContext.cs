fa15c297dd5bf2149288ad5181b2e4f84c1345d6
Carlos Andres Chisco Rodriguez <cchisco@minsalud.gov.co>
2022-09-21 19:16:22 -0500

    Add project files.

---
+++ Prueba.Entity/ToughBuiltContext.cs
@@ -0,0 +1,16 @@
+﻿using Prueba.Entity.Entities;
+using System.Data.Entity;
+
+namespace Prueba.Entity
+{
+    internal class ToughBuiltContext : DbContext
+    {
+        private const string connectionString = "Data Source=PREDATOR;Initial Catalog=ToughBuilt;Integrated Security=true";
+
+        public ToughBuiltContext():base(connectionString)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
