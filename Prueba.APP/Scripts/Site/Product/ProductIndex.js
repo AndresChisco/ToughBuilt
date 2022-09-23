fba08edcdcdf5ba6ba82a20e6bd16801f31d88eb
Carlos Andres Chisco Rodriguez <carlos_chisco@hotmail.com>
2022-09-23 07:31:58 -0500

    quitando comentarios

--- Prueba.APP/Scripts/Site/Product/ProductIndex.js
+++ Prueba.APP/Scripts/Site/Product/ProductIndex.js
@@ -11,30 +11,6 @@ $(document).ready(function () {
     );
 });
 
-//$("#tableProducts").on("click", "tr", async function () {
-//    var id = parseInt(productTable.row(this).data()[1]);
-//    var datosProducto = await getProduct(id);
-//    var images = datosProducto.FeaturedImages.split(';');
-//    $(".carousel-inner").html("");
-//    var active = "";
-//    $.each(images, function (index, value) {
-//        if (index === 0)
-//            active = "active"
-//        else
-//            active = "";
-//        var imagesCarousel = $(".carousel-inner").html();
-//        var newImage = '<div class="carousel-item ' + active + '"><img class="d-block w-100" src="/Resources/Images/Products/' + datosProducto.Name + '/' + value + '" alt = "' + value + '" style="width: 450px; height: 500px" /></div>'
-//        $(".carousel-inner").html(imagesCarousel + newImage);
-//        $('.carousel').carousel(); 
-//    });
-//    $("#ProductDetailsTitle").text(datosProducto.Name);
-//    $("#txtDescription").text(datosProducto.Description);
-//    $("#txtCharacteristics").text(datosProducto.FeaturedCharacteristics);
-//    $("#txtPrice").text(datosProducto.Price);
-//    $("#txtStock").text(datosProducto.Stock);
-//    $('#ProductDetailsModal').modal("show");
-//});
-
 $(".modalTrigger").on("click", async function () {
 
     var id = $(this).data("id");
